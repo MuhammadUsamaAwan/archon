99e265d5f2fd034d59f15d752ca910737d01cda6
MuhammadUsamaAwan <underusama@gmail.com>
2025-05-23 01:13:07 +0500

    feat: api client

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -3,10 +3,12 @@ import { serveStatic } from 'hono/bun';
 
 const app = new Hono();
 
-app.basePath('/api').get('/', c => {
-  return c.text('Hello Hono!');
+const route = app.basePath('/api').get('/', c => {
+  return c.json({ message: 'Hello from API!' });
 });
 
 app.use('/assets/*', serveStatic({ root: './client' })).get('*', serveStatic({ path: './client/index.html' }));
 
 export default app;
+
+export type AppType = typeof route;
--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,3 +1,15 @@
+import { client } from '@app/api-client';
+import { useEffect } from 'react';
+
 export function App() {
+  useEffect(() => {
+    async function fetchData() {
+      const res = await client.api.$get();
+      const data = await res.json();
+      console.log(data.message);
+    }
+    fetchData();
+  }, []);
+
   return <div>app</div>;
 }
