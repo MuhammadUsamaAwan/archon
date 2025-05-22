3670a3c16165e33f7dbf751174252b3e0220c221
MuhammadUsamaAwan <underusama@gmail.com>
2025-05-22 23:47:43 +0500

    chore: biome organize imports

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import { App } from './app';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLDivElement).render(
   <StrictMode>
     <App />
   </StrictMode>
