e3544c38f11e89dd89913575e38e9b30c7826f90
MuhammadUsamaAwan <underusama@gmail.com>
2025-06-03 19:07:59 +0500

    fix: api client type issue

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 
 const app = new Hono();
-const routes = app.get('/', c => c.json({ message: 'Hello from Api!' }));
+const routes = app.get('/', c => c.json({ message: 'Hello from API!' }));
 
 export type AppType = typeof routes;
 
