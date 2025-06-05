19b4659482485e9898f5bbb2d6268944bce41c61
MuhammadUsamaAwan <underusama@gmail.com>
2025-06-05 12:12:28 +0500

    chore(web): add mantine

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,11 +1,20 @@
+import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 import { StrictMode } from 'react';
-
 import { createRoot } from 'react-dom/client';
-
 import { App } from './app';
+import { theme } from './lib/theme';
+
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/notifications/styles.css';
+import '~/web/styles/global.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+      <Notifications position='top-right' />
+    </MantineProvider>
   </StrictMode>
 );
